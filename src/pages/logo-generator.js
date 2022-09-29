54adb7a34e26e348fcdbe26cf5e33f7567024bbe
Will McLean <willmcl@gmail.com>
2022-09-29 10:34:49 +0100

    Rotate the container.

--- src/pages/logo-generator.js
+++ src/pages/logo-generator.js
@@ -26,7 +26,9 @@ function DataVis() {
       Common = Matter.Common,
       MouseConstraint = Matter.MouseConstraint,
       Mouse = Matter.Mouse,
-      Composite = Matter.Composite,
+      Events = Matter.Events,
+      Things = Matter.Composite,
+      WashingMachine = Matter.Composite,
       Bodies = Matter.Bodies;
 
     // create engine
@@ -54,19 +56,26 @@ function DataVis() {
 
     // Add bodies
     let stack = Composites.stack(200, 250, 10, 5, 0, 0, function (x, y) {
-      return Bodies.rectangle(x, y, 20, 20);
+      return Bodies.rectangle(x, y, 30, 30);
     });
 
-    Composite.add(world, stack);
+    Things.add(world, stack);
 
     // Add walls
-    Composite.add(world, [
+    WashingMachine.add(world, [
       // walls
       Bodies.rectangle(300, 100, 400, 10, {isStatic: true}),
       Bodies.rectangle(300, 500, 400, 10, {isStatic: true}),
       Bodies.rectangle(500, 300, 10, 400, {isStatic: true}),
       Bodies.rectangle(100, 300, 10, 400, {isStatic: true})
     ]);
+
+    let center = Matter.Vector.create(300,300)
+
+    Events.on(runner, 'afterTick', function(event) {
+      // Body.setAngularVelocity(compound, 0.02);
+      WashingMachine.rotate(world, 0.002, center);
+    });
 
   }, []);
 
