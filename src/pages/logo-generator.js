3df6dbc7ff14c558966dbfed0dbad7a44ab52f44
Will McLean <willmcl@gmail.com>
2022-09-29 10:08:32 +0100

    Add bodies to a washing machine container for logo generator.

--- src/pages/logo-generator.js
+++ src/pages/logo-generator.js
@@ -15,53 +15,65 @@ const CanvasHolder = styled.div`
 
 function DataVis() {
 
-  const boxRef = useRef(null);
+  const holderRef = useRef(null);
   const canvasRef = useRef(null);
 
   useEffect(() => {
-    let Engine = Matter.Engine;
-    let Render = Matter.Render;
-    let World = Matter.World;
-    let Bodies = Matter.Bodies;
+    let Engine = Matter.Engine,
+      Render = Matter.Render,
+      Runner = Matter.Runner,
+      Composites = Matter.Composites,
+      Common = Matter.Common,
+      MouseConstraint = Matter.MouseConstraint,
+      Mouse = Matter.Mouse,
+      Composite = Matter.Composite,
+      Bodies = Matter.Bodies;
 
-    let engine = Engine.create({});
+    // create engine
+    let engine = Engine.create(),
+      world = engine.world;
 
+    // create renderer
     let render = Render.create({
-      element: boxRef.current,
+      element: holderRef.current,
       engine: engine,
       canvas: canvasRef.current,
       options: {
         width: 600,
         height: 600,
-        background: 'rgba(255, 0, 0, 0.5)',
-        wireframes: false
+        showAngleIndicator: true,
       }
     });
 
-    const floor = Bodies.rectangle(300, 590, 600, 20, {
-      isStatic: true,
-      render: {
-        fillStyle: 'blue'
-      }
-    });
-
-    const ball = Bodies.circle(150, 0, 30, {
-      restitution: 0.9,
-      render: {
-        fillStyle: 'yellow'
-      }
-    });
-
-    World.add(engine.world, [floor, ball]);
-
-    Engine.run(engine);
     Render.run(render);
+
+    // create runner
+    // https://brm.io/matter-js/docs/classes/Runner.html
+    let runner = Runner.create();
+    Runner.run(runner, engine);
+
+    // Add bodies
+    let stack = Composites.stack(200, 250, 10, 5, 0, 0, function (x, y) {
+      return Bodies.rectangle(x, y, 20, 20);
+    });
+
+    Composite.add(world, stack);
+
+    // Add walls
+    Composite.add(world, [
+      // walls
+      Bodies.rectangle(300, 100, 400, 10, {isStatic: true}),
+      Bodies.rectangle(300, 500, 400, 10, {isStatic: true}),
+      Bodies.rectangle(500, 300, 10, 400, {isStatic: true}),
+      Bodies.rectangle(100, 300, 10, 400, {isStatic: true})
+    ]);
+
   }, []);
 
   return (
     <>
       <Seo title="Logo generator" />
-      <CanvasHolder ref={boxRef}>
+      <CanvasHolder ref={holderRef}>
         <canvas ref={canvasRef} />
       </CanvasHolder>
     </>
