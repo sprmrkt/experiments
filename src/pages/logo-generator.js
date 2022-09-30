cbe7cb21a9761f436e0def842071a12bd4721a03
Will McLean <willmcl@gmail.com>
2022-09-30 14:38:09 +0100

    Update dependencies.

--- src/pages/logo-generator.js
+++ src/pages/logo-generator.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useRef} from 'react';
 import Seo from "../components/molecules/Seo";
 import styled from "styled-components";
 import Matter from 'matter-js';
