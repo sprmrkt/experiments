42747b460a7dddf88d21a89b1ee0c39fa21603ab
Will McLean <willmcl@gmail.com>
2022-09-30 10:27:19 +0100

    Attach sprites to the compound body.

--- src/pages/logo-generator.js
+++ src/pages/logo-generator.js
@@ -9,8 +9,12 @@ const CanvasHolder = styled.div`
   height: var(--windowHeight);
   overflow: hidden;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+  button {
+    margin: 10px 0;
+  }
 `;
 
 function DataVis() {
@@ -30,6 +34,8 @@ function DataVis() {
       Composite = Matter.Composite,
       Svg = Matter.Svg,
       Vertices = Matter.Vertices,
+      Body = Matter.Body,
+      Constraint = Matter.Constraint,
       Bodies = Matter.Bodies;
 
     // // provide concave decomposition support library
@@ -47,7 +53,8 @@ function DataVis() {
       options: {
         width: 600,
         height: 600,
-        showAngleIndicator: true,
+        wireframes: false,
+        // showAngleIndicator: true,
       }
     });
 
@@ -78,68 +85,123 @@ function DataVis() {
 
 
       ([
-        './logo-gen/s.svg',
-        './logo-gen/u.svg',
-        './logo-gen/p-fill.svg',
-        './logo-gen/e.svg',
-        './logo-gen/r-fill.svg',
-        './logo-gen/m.svg',
-        './logo-gen/a-fill.svg',
-        './logo-gen/r-fill.svg',
-        './logo-gen/k.svg',
-        './logo-gen/e.svg',
-        './logo-gen/t.svg',
-      ]).forEach(function (path, i) {
-        loadSvg(path).then(function (root) {
-          console.log(root)
-          let color = Common.choose(['#f19648', '#f5d259', '#f55a3c', '#063e7b', '#ececd1']);
+        {svg: './logo-gen/svg/s.svg', png: './logo-gen/png/s.png', offset:{x:0,y:0}},
+        {svg: './logo-gen/svg/u.svg', png: './logo-gen/png/u.png', offset:{x:0,y:3}},
+        {svg: './logo-gen/svg/p.svg', png: './logo-gen/png/p.png', offset:{x:-5,y:-9}},
+        {svg: './logo-gen/svg/e.svg', png: './logo-gen/png/e.png', offset:{x:-3,y:0}},
+        {svg: './logo-gen/svg/r.svg', png: './logo-gen/png/r.png', offset:{x:-2,y:-2}},
+        {svg: './logo-gen/svg/m.svg', png: './logo-gen/png/m.png', offset:{x:0,y:0}},
+        {svg: './logo-gen/svg/a.svg', png: './logo-gen/png/a.png', offset:{x:0,y:4}},
+        {svg: './logo-gen/svg/r.svg', png: './logo-gen/png/r2.png', offset:{x:-2,y:-2}},
+        {svg: './logo-gen/svg/k.svg', png: './logo-gen/png/k.png', offset:{x:-8,y:0}},
+        {svg: './logo-gen/svg/e.svg', png: './logo-gen/png/e2.png', offset:{x:-3,y:0}},
+        {svg: './logo-gen/svg/t.svg', png: './logo-gen/png/t.png', offset:{x:0,y:-11}},
+      ]).forEach(function (letter, i) {
+        loadSvg(letter.svg).then(function (root) {
+          // let color = Common.choose(['#f19648', '#f5d259', '#f55a3c', '#063e7b', '#ececd1']);
 
           let vertexSets = select(root, 'path')
             .map(function (path) {
-              return Vertices.scale(Svg.pathToVertices(path, 5), 2, 2);
+              return Vertices.scale(Svg.pathToVertices(path, 3), 2, 2);
             });
 
-          console.log(vertexSets)
+          let x = i % 4 * 75 + 175
+          let y = Math.floor(i / 4) * 75 + 175
 
-
-          Composite.add(world, Bodies.fromVertices(150 + i * 30, 300, vertexSets, {
+          let body = Bodies.fromVertices(x, y, vertexSets, {
             render: {
-              fillStyle: color,
-              strokeStyle: color,
-              lineWidth: 1
+              fillStyle: 'none',
+              strokeStyle: 'none',
+              lineWidth: 1,
+              slop: 0,
+              visible: false,
+            },
+          }, true)
+          let spriteHolder = Bodies.rectangle(
+            body.bounds.min.x,
+            body.bounds.min.y,
+            (body.bounds.max.x - body.bounds.min.x),
+            (body.bounds.max.y - body.bounds.min.y),
+            {
+              collisionFilter: {
+                mask: 0,
+              },
+              render: {
+                fillStyle: 'none',
+                strokeStyle: '#ffffff',
+                sprite: {
+                  texture: letter.png,
+                  xOffset: 0,
+                  yOffset: 0,
+                }
+              }
             }
-          }, true))
+          )
+          let constraint = Constraint.create({
+            bodyA: body,
+            pointA: {x: 0, y: 10},
+            bodyB: spriteHolder,
+            pointB: {x: 0 + letter.offset.x, y: 10 + letter.offset.y},
+            length: 0,
+            render: {
+              visible: false
+            }
+          });
+          let constraint2 = Constraint.create({
+            bodyA: body,
+            pointA: {x: 0, y: -10},
+            bodyB: spriteHolder,
+            pointB: {x: 0 + letter.offset.x, y: -10 + letter.offset.y},
+            length: 0,
+            render: {
+              visible: false
+            }
+          });
+
+          let group = Composite.create({label: `group-${i}`});
+          Composite.add(group, [body, spriteHolder, constraint, constraint2])
+          Composite.add(world, group)
 
 
         });
       });
 
-
-      // let stack = Composites.stack(200, 250, 10, 5, 0, 0, function (x, y) {
-      //   return Bodies.rectangle(x, y, 30, 30);
-      // });
-      // Composite.add(world, stack);
-
     } else {
       Common.warn('Fetch is not available. Could not load SVG.');
     }
-    
+
     // Add walls
     // let washingMachine = Composite.create([{label: 'washingMachine'}])
-    let washingMachine = Composite.add(world, [
+    let washingMachine = Composite.create({label: 'Washing Machine'});
+    Composite.add(washingMachine, [
       // walls
-      Bodies.rectangle(300, 100, 400, 10, {isStatic: true}),
-      Bodies.rectangle(300, 500, 400, 10, {isStatic: true}),
-      Bodies.rectangle(500, 300, 10, 400, {isStatic: true}),
-      Bodies.rectangle(100, 300, 10, 400, {isStatic: true})
+      Bodies.rectangle(300, 100, 400, 50, {isStatic: true, slop: 0, render: { visible: false }}),
+      Bodies.rectangle(300, 500, 400, 50, {isStatic: true, slop: 0, render: { visible: false }}),
+      Bodies.rectangle(500, 300, 50, 400, {isStatic: true, slop: 0, render: { visible: false }}),
+      Bodies.rectangle(100, 300, 50, 400, {isStatic: true, slop: 0, render: { visible: false }})
     ]);
+    Composite.add(world, washingMachine)
 
-    let center = Matter.Vector.create(300, 300)
+    // add mouse control
+    let mouse = Mouse.create(render.canvas),
+      mouseConstraint = MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+          stiffness: 0.2,
+          render: {
+            visible: false
+          }
+        }
+      });
+
+    Composite.add(world, mouseConstraint);
+
 
     // Set some base variables for the rotation animation
+    let center = Matter.Vector.create(300, 300)
     let counter = 0;
     let whereItIs = 0;
-    let interval = 6; // Seconds for the interval. The animation will use half of this and then we will pause for half
+    let interval = 8; // Seconds for the interval. The animation will use half of this and then we will pause for 3/4 of it
 
     // Our easing function for the washing machine animation
     let bowEasing = (x, timeFraction) => {
@@ -153,7 +215,7 @@ function DataVis() {
       counter += 1;
 
       // Find out where the rotation needs be from 0 to 90deg
-      let whereItShouldBe = Math.PI / 2 * Math.min(1, bowEasing(1.5, counter / 60 / (interval / 2)));
+      let whereItShouldBe = Math.PI / 2 * Math.min(1, bowEasing(1.5, counter / 60 / (interval / 4)));
       // The increment we need to use to rotate the composite is where it should be minus where it is.
       // Find that and use it on the washing machine composite.
       let increment = whereItShouldBe - whereItIs;
