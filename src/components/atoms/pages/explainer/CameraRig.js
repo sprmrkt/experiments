b42e98be9228e36370d9cc0ec3810d76e430c0e1
Will McLean <willmcl@gmail.com>
2022-10-11 17:50:57 +0100

    First camera zoom for explainer.

--- src/components/atoms/pages/explainer/CameraRig.js
+++ src/components/atoms/pages/explainer/CameraRig.js
@@ -7,9 +7,12 @@ function CameraRig() {
   const data = useScroll()
 
   useFrame((state) => {
+    const r0 = data.range(0, 0.1)
     const r1 = data.range(0.1, 0.3)
     const r2 = data.range(0.5, 0.3)
-    if(r1 < 1) {
+    if(r0 < 1) {
+      state.camera.position.z = THREE.MathUtils.lerp(0, 3.25, r0)
+    } else if(r1 < 1) {
       state.camera.position.x = THREE.MathUtils.lerp(0, 0.95, r1)
     } else if(r2 < 1) {
       state.camera.position.x = THREE.MathUtils.lerp(0.95, 1.9, r2)
