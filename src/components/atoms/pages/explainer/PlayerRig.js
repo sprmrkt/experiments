c9bb276d5f7fc563d687b4883b43ab7bbcf045f5
Will McLean <willmcl@gmail.com>
2022-10-20 12:20:40 +0100

    Remove floating player movement.

--- src/components/atoms/pages/explainer/PlayerRig.js
+++ src/components/atoms/pages/explainer/PlayerRig.js
@@ -7,45 +7,43 @@ import {useExplainerStore} from "../../../../utils/myStore";
 import {useXR} from "@react-three/xr";
 
 const playerPos = [
-  [0, -15, -22],
-  [0, 5, -22],
-  [0, 5, 17],
-  [10, 5, 17],
-  [-10, 5, 17],
-  [-10, 13, 17],
-  [0, 13, 17],
-  [10, 13, 17],
+  [0, -15, -22, Math.PI],
+  [0, 5, -22, Math.PI],
+  [0, 5, 17, 0],
+  [10, 5, 17, 0],
+  [-10, 5, 17, 0],
+  [-10, 13, 17, 0],
+  [0, 13, 17, 0],
+  [10, 13, 17, 0],
 ]
 
 function PlayerRig() {
   const scene = useExplainerStore((state) => state.scene)
-  const { player, isPresenting } = useXR()
+  const {player, isPresenting} = useXR()
 
   // useEffect(() => {
   //   player.rotation.y = Math.PI
   // });
 
   useEffect(() => {
-    if (isPresenting) {
-      player.rotation.y = Math.PI
-      player.position.x = playerPos[0][0]
-      player.position.y = playerPos[0][1]
-      player.position.z = playerPos[0][2]
-    }
-  }, [isPresenting])
+    player.rotation.y = Math.PI
+    player.position.x = playerPos[scene][0]
+    player.position.y = playerPos[scene][1]
+    player.position.z = playerPos[scene][2]
+  }, [scene])
 
-  useFrame((state,delta ) => {
-    let x = THREE.MathUtils.damp(player.position.x, playerPos[scene][0], 0.075, delta)
-    let y = THREE.MathUtils.damp(player.position.y, playerPos[scene][1], 0.075, delta)
-    let z = THREE.MathUtils.damp(player.position.z, playerPos[scene][2], 0.075, delta)
-    // let x = THREE.MathUtils.lerp(player.position.x, playerPos[scene][0], 0.001)
-    // let y = THREE.MathUtils.lerp(player.position.y, playerPos[scene][1], 0.001)
-    // let z = THREE.MathUtils.lerp(player.position.z, playerPos[scene][2], 0.001)
-    // console.log(x,y,z)
-    player.position.x = x
-    player.position.y = y
-    player.position.z = z
-  })
+  // useFrame((state,delta ) => {
+  //   let x = THREE.MathUtils.damp(player.position.x, playerPos[scene][0], 0.075, delta)
+  //   let y = THREE.MathUtils.damp(player.position.y, playerPos[scene][1], 0.075, delta)
+  //   let z = THREE.MathUtils.damp(player.position.z, playerPos[scene][2], 0.075, delta)
+  //   // let x = THREE.MathUtils.lerp(player.position.x, playerPos[scene][0], 0.001)
+  //   // let y = THREE.MathUtils.lerp(player.position.y, playerPos[scene][1], 0.001)
+  //   // let z = THREE.MathUtils.lerp(player.position.z, playerPos[scene][2], 0.001)
+  //   // console.log(x,y,z)
+  //   player.position.x = x
+  //   player.position.y = y
+  //   player.position.z = z
+  // })
 
   return null;
 }
